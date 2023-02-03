583c53adcdb9c3fc11c628d21e2fd8fbb60323eb
Eunjae Lee <hey@eunjae.dev>
2023-02-03 17:07:12 +0100

    feat: wip

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,65 @@
+import {mkdirp} from 'mkdirp'
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import frontmatter from 'front-matter'
+import fs from 'fs'
+import path from 'path'
+import {simpleGit} from 'simple-git'
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+  const destPath: string = core.getInput('dest')
+  const githubToken: string = core.getInput('github_token')
+  const extension: string = core.getInput('extension')
+  const commitPrefix: string = core.getInput('commit_prefix')
+  const slugAsFolderName: boolean = core.getBooleanInput('slug_as_folder_name')
+  console.log('TEST', {
+    destPath,
+    githubToken,
+    extension,
+    commitPrefix,
+    slugAsFolderName,
+    GITHUB_ACTOR: process.env.GITHUB_ACTOR,
+    GITHUB_REPOSITORY: process.env.GITHUB_REPOSITORY
+  })
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+  const issue = github.context.payload.issue
+  if (!issue) {
+    core.setFailed('This Action works only from the `issue` triggers.')
+    return
   }
+
+  const {
+    attributes: {slug}
+  } = frontmatter<{slug: string}>(issue.body || '')
+
+  const relativePath = path.join(
+    destPath,
+    slugAsFolderName ? slug || String(issue.number) : String(issue.number),
+    `index${extension}`
+  )
+  const fullPath = path.resolve(relativePath)
+  mkdirp.sync(path.dirname(fullPath))
+  fs.writeFileSync(fullPath, issue.body || '')
+
+  const ORGANIZATION_DOMAIN = 'github.com'
+  const {GITHUB_ACTOR, GITHUB_TOKEN, GITHUB_REPOSITORY} = process.env
+
+  const git = simpleGit()
+  git
+    .addRemote(
+      'origin',
+      `https://${GITHUB_ACTOR}:${
+        githubToken || GITHUB_TOKEN
+      }@${ORGANIZATION_DOMAIN}/${GITHUB_REPOSITORY}`
+    )
+    .addConfig('user.name', GITHUB_ACTOR || 'github-actions')
+    .addConfig(
+      'user.email',
+      `${GITHUB_ACTOR}@users.noreply.${ORGANIZATION_DOMAIN}`
+    )
+    .add('./*')
+    .commit(`${commitPrefix} update ${relativePath}`)
+    .push('origin', 'main')
 }
 
 run()

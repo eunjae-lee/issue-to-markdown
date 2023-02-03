0f449326134aed456750b62eb71066f4c6d81b78
Eunjae Lee <hey@eunjae.dev>
2023-02-03 17:07:12 +0100

    feat: wip

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,55 @@
+import {execaCommandSync} from 'execa'
+import {mkdirp} from 'mkdirp'
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import frontmatter from 'front-matter'
+import fs from 'fs'
+import path from 'path'
+
+function runCommand(command: string): void {
+  console.log(execaCommandSync(command).stdout)
+}
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+  const destPath: string = core.getInput('dest')
+  const githubToken: string = core.getInput('github_token')
+  const extension: string = core.getInput('extension')
+  const commitPrefix: string = core.getInput('commit_prefix')
+  const slugAsFolderName: boolean = core.getBooleanInput('slug_as_folder_name')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+  const issue = github.context.payload.issue
+  if (!issue) {
+    core.setFailed('This Action works only from the `issue` triggers.')
+    return
   }
+
+  const {
+    attributes: {slug}
+  } = frontmatter<{slug: string}>(issue.body || '')
+
+  const fullPath = path.resolve(
+    destPath,
+    slugAsFolderName ? slug : String(issue.number),
+    `index${extension}`
+  )
+  mkdirp.sync(path.dirname(fullPath))
+  fs.writeFileSync(fullPath, issue.body || '')
+
+  const ORGANIZATION_DOMAIN = 'github.com'
+  const {GITHUB_ACTOR, GITHUB_TOKEN, GITHUB_REPOSITORY} = process.env
+
+  runCommand(
+    `git remote set-url origin "https://${GITHUB_ACTOR}:${
+      githubToken || GITHUB_TOKEN
+    }@${ORGANIZATION_DOMAIN}/${GITHUB_REPOSITORY}"`
+  )
+  runCommand(`git config --global user.name "${GITHUB_ACTOR}"`)
+  runCommand(
+    `git config --global user.email "${GITHUB_ACTOR}@users.noreply.${ORGANIZATION_DOMAIN}"`
+  )
+  runCommand(`git add -A`)
+  runCommand(`git commit -m "${commitPrefix} update ${fullPath}"`)
+  runCommand(`git push origin main`)
 }
 
 run()

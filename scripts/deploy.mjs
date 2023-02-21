9c04cce39e9a5b89baba117777fd86aab1e76824
Eunjae Lee <hey@eunjae.dev>
2023-02-21 15:56:56 +0100

    chore: use yarn

--- scripts/deploy.mjs
+++ scripts/deploy.mjs
@@ -4,18 +4,18 @@ import {$} from 'zx'
 const version = process.argv[3]
 if (!version) {
   console.error('Version is missing.')
-  console.log('  e.g. > npm run deploy 0.0.1')
+  console.log('  e.g. > yarn deploy 0.0.1')
   process.exit(1)
 }
 
 if (!/^\d+\.\d+\.\d+$/.test(version)) {
   console.error('Version is malformed.')
-  console.log('  e.g. > npm run deploy 0.0.1')
+  console.log('  e.g. > yarn deploy 0.0.1')
   process.exit(1)
 }
 
 await $`npm pkg set version=${version}`
-await $`npm run all`
+await $`yarn all`
 await $`git add .`
 await $`git commit -m "chore: release v${version}"`
 
